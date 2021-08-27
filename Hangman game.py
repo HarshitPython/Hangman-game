153d869e577a55cda7ed1483b6a449d5effc1b3e
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-27 21:01:19 +0530

    Update Hangman game.py

--- Hangman game.py
+++ Hangman game.py
@@ -31,6 +31,7 @@ while not game_is_finished:
     #Loop through each position in the chosen_word    
     for position in range(word_length):
         letter = chosen_word[position]
+        
         if letter == guess:
             display[position] = letter
     print(f"{' '.join(display)}")
