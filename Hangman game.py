5abd8c9c1c6b50c9f93697fccefb17e3e0742511
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-27 20:58:11 +0530

    Update Hangman game.py

--- Hangman game.py
+++ Hangman game.py
@@ -28,7 +28,7 @@ while not game_is_finished:
     if guess in display:
         print(f"You've already guessed {guess}")
 
-        
+    Loop through each position in the chosen_word    
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
