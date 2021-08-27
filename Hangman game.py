cef776a9c95ab1aebef0e6c438380280e1103cc4
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-27 20:51:32 +0530

    Update Hangman game.py

--- Hangman game.py
+++ Hangman game.py
@@ -16,6 +16,8 @@ for _ in range(word_length):
     display += "_"
 
 while not game_is_finished:
+    
+    
     guess = input("Guess a letter: ").lower()
 
     #Use the clear() function imported from replit to clear the output between guesses.
